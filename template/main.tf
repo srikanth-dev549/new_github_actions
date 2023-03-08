7c9a8c70fdfe8c7b971c2d2a8c511da427fef208
Phani V <123108378+srikanth-dev549@users.noreply.github.com>
2023-03-08 13:02:28 +0530

    Create main.tf

---
+++ template/main.tf
@@ -0,0 +1,35 @@
+locals {
+  json_data_7 = jsondecode(file("./${var.data_file}"))
+}
+
+locals {
+
+  helper_list = flatten([for v in local.json_data_7.user_roles:
+            [ for project, role in v:
+            [ for roles, users in role:
+            [ for i in users:
+             { "project" = project
+                "id" = "${project}-${roles}-${i}"
+               "role" = roles
+               "member" = i
+              }
+            ]
+            ]
+            ]
+  ]
+          )
+}
+
+resource "google_project_iam_member" "rolebinding" {
+  for_each     = { for idx, v in local.helper_list: v.id => v }
+  project = each.value.project
+  role    = "roles/${each.value.role}"
+  member  = each.value.member
+  
+}
+
+  resource "null_resource" "display_message" {
+  provisioner "local-exec" {
+    command = "echo '##############APPLYING ONLY SERVICE ACCOUNTS AND USERS TO THE ROLES IN PROJECT LEVEL || ORG LEVEL POLICIES ARE OMITTED############################'"
+  }
+}
