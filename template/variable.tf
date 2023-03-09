8511322d1e932db4fad73aaf6e91fad0ecaadff7
Phani V <123108378+srikanth-dev549@users.noreply.github.com>
2023-03-09 12:30:36 +0530

    Update variable.tf

--- template/variable.tf
+++ template/variable.tf
@@ -2,3 +2,9 @@ variable "data_file" {
     type = string
     default = "$PROJECT_NAME-data.json"
 }
+
+variable "bucket_name" {
+    type = string
+    default = "$PROJECT_NAME-STATE-FILE-IAM-ROLES"
+}
+
