017c22c3251dad3af58aba80a475d9341bbe75ed
Phani V <123108378+srikanth-dev549@users.noreply.github.com>
2023-03-21 12:19:49 +0530

    Update main.py

--- template/main.py
+++ template/main.py
@@ -3,7 +3,7 @@ import json
 import subprocess
 import os
 # Project IDs are stored in the list format
-project_id = "new-project2-376506"
+project_id = "$PROJECT_NAME"
 
 all_projects_roles_principles = [] # Storing all the projects with roles and principles as a list
 project_to_users = {} # Dictionary to map project ID as key and user_role
